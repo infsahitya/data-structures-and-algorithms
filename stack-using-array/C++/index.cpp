7c40f6025528b1b9c84ef6b9f5f77ba321cc1a8d
infsahitya <sahitya.api@gmail.com>
2023-12-21 10:45:33 +0530

    update

--- stack-using-array/C++/index.cpp
+++ stack-using-array/C++/index.cpp
@@ -22,12 +22,12 @@ int main()
 
       std::cout << std::endl
                 << std::endl;
-      std::cout << "1. Press (D) to display all the elements." << std::endl;
-      std::cout << "2. Press (I) to push a new element." << std::endl;
-      std::cout << "3. Press (P) to pop an element." << std::endl;
-      std::cout << "4. Press (C) to get the current size." << std::endl;
-      std::cout << "5. Press (T) to get the top element." << std::endl;
-      std::cout << "6. Press (E) to exit the program." << std::endl;
+      std::cout << "1. Press (D or d) to display all the elements." << std::endl;
+      std::cout << "2. Press (I or i) to push a new element." << std::endl;
+      std::cout << "3. Press (P or p) to pop an element." << std::endl;
+      std::cout << "4. Press (C or c) to get the current size." << std::endl;
+      std::cout << "5. Press (T or t) to get the top element." << std::endl;
+      std::cout << "6. Press (E or e) to exit the program." << std::endl;
       std::cout << "Enter your choice: ";
       std::cin >> userChoice;
       std::cout << std::endl;
